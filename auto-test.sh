eebdc9f3cb6c697244b8ea2e87475d87900f09c1
Phule <syphua2@gmail.com>
2022-11-25 18:21:36 +0700

    Update auto-test.sh

--- auto-test.sh
+++ auto-test.sh
@@ -1,12 +1,12 @@
-cd releases-auto-test 
+cd releases-plus 
 ls -lt | tail -n +5 | awk "{print $9}" | xargs rm -rf
 git clone git@github.com:Real-Authentication/BFS_RLA.git RA_$(date +"%Y%m%d%s")
 cd $(ls -td -- */ | head -n 1)
 git checkout qc
 git pull
-cp ~/environment-auto-test/.env www/
-cp ~/environment-auto-test/.env.frontend www/front/.env
-cp ~/environment-auto-test/oauth-* www/storage/
+cp ~/environment-plus/.env www/
+cp ~/environment-plus/.env.frontend www/front/.env
+cp ~/environment-plus/oauth-* www/storage/
 cd www/front
 npm install
 npm run build-prod:main
@@ -24,8 +24,8 @@ php artisan migrate
 php artisan config:clear
 composer customer-api-test
 cd ~
-rm app.realauthentication.com-test
-ln -sf "$(ls -1dt releases-auto-test/*/ | sed 1q)" app.realauthentication.com-test
+rm app.realauthentication.com-plus
+ln -sf "$(ls -1dt releases-auto-test/*/ | sed 1q)" app.realauthentication.com-plus
 echo QSr%@B{7bME3@6L | sudo -S service php7.3-fpm restart
 echo QSr%@B{7bME3@6L | sudo supervisorctl stop horizon
 echo QSr%@B{7bME3@6L | sudo supervisorctl start horizon
